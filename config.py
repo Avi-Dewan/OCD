a32b46d3190b7e613b8f4cb3c1f217fa600f3800
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-04-03 04:58:20 +0600

    initial

---
+++ config.py
@@ -0,0 +1,18 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+cifar_10_root = '/data/cifar10'
+cifar_100_root = '/data/cifar100'
+cub_root = '/data/cub'
+aircraft_root = '/data/aircraft'
+herbarium_dataroot = '/data/herb'
+imagenet_root = '/data/ImageNet'
+
+# OSR Split dir
+osr_split_dir = '/home/OCD/data/ssb_splits'
+
+# -----------------
+# OTHER PATHS
+# -----------------
+dino_pretrain_path = '/home/OCD/pretrained_models/dino_vitbase16_pretrain.pth'
+exp_root = '/home/duruoyi/OCD/'          # All logs and checkpoints will be saved here
